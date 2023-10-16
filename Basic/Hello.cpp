c62e65cc9aa597d4f14c95cfdb6ac77a969ccc42
Sonu_Yadav <134956732+SonuYadav143@users.noreply.github.com>
2023-10-16 05:01:27 +0000

    update

---
+++ Basic/Hello.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"welcome to hello page1"<<endl;
+    cout<<"welcome to hello page2"<<endl;
+    cout<<"welcome to hello page3"<<endl;
+}
---
+++ Basic/UserInput.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+string name;
+int age;
+float height;
+cout<<"Enter Your Name:-"<<endl;
+cin>>name;
+cout<<"Enter Your age"<<endl;
+cin>>age;
+cout<<"Enter Your Height"<<endl;
+cin>>height;
+
+cout<<"My name is : "<<name<<endl;
+cout<<"My name is : "<<age<<endl;
+cout<<"My name is : "<<height<<endl;
+}
